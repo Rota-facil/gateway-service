d537638a7929b61dda8947ecddf644d19ee4d8d4
Thauã Gabriel <tagashi.dev@gmail.com>
2026-05-31 22:18:40 -0300

    feat: add cors in security config

--- src/main/java/com/rota/facil/gateway_service/security/config/SecurityConfig.java
+++ src/main/java/com/rota/facil/gateway_service/security/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(cors -> {})
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
