7b6277f4451ab91677e98bcfdd008e9608828d02
Thauã Gabriel <tagashi.dev@gmail.com>
2026-07-08 06:53:59 -0300

    feat: protect endpoint to generate and list heat-map

--- src/main/java/com/rota/facil/gateway_service/security/config/SecurityConfig.java
+++ src/main/java/com/rota/facil/gateway_service/security/config/SecurityConfig.java
@@ -50,6 +50,8 @@ public class SecurityConfig {
                         .pathMatchers("/places/**").hasAnyRole("ADMIN", "SUPERUSER")
                         .pathMatchers("/audit/**").hasAnyRole("ADMIN", "SUPERUSER")
                         .pathMatchers("/transports/metrics/**").hasAnyRole("ADMIN", "SUPERUSER")
+                        .pathMatchers(HttpMethod.POST, "/transports/routes/{routeId}/board-point/heat-map").hasAnyRole("ADMIN", "SUPERUSER")
+                        .pathMatchers(HttpMethod.GET, "/files/heat-map/{routeId}/all").hasAnyRole("ADMIN", "SUPERUSER")
                         .pathMatchers(HttpMethod.GET, "/transports/institutions/route-counts").hasAnyRole("ADMIN", "SUPERUSER")
 
                         .pathMatchers("/auth/user/prefecture/register").hasRole("SUPERUSER")
